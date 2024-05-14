64c329a0f18258c00d32384eb10fb230052908c8
FedericoGarza <fede.garza.ramirez@gmail.com>
2024-05-14 14:20:07 -0700

    fix: add ms as freq

--- src/timesfm/timesfm.py
+++ src/timesfm/timesfm.py
@@ -69,7 +69,7 @@ def freq_map(freq: str):
         or freq.endswith("U")
     ):
         return 0
-    elif freq.endswith("W") or freq.endswith("M") or freq in ["W-MON"]:
+    elif freq.endswith("W") or freq.endswith("M") or freq in ["W-MON", "MS"]:
         return 1
     elif freq.endswith("Y") or freq.endswith("Q"):
         return 2
